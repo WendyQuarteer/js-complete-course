56f4365e18b7502e571d1cd96b61a46161c01085
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-05 15:01:22 +0200

    ex2.1

---
+++ 02-colors/01-change-bcg-one/script.js
@@ -0,0 +1,27 @@
+/* becode/javascript
+ *
+ * /03-colors/01-change-bcg-one/script.js - 3.1: couleur de fond (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+document.getElementById("red").onclick = function onClickButton() {
+
+        document.body.style.background = "red";
+};
+document.getElementById("green").onclick = function onClickButton() {
+
+        document.body.style.background = "green";
+};
+document.getElementById("yellow").onclick = function onClickButton() {
+
+        document.body.style.background = "yellow";
+};
+document.getElementById("blue").onclick = function onClickButton() {
+
+        document.body.style.background = "blue";
+};
