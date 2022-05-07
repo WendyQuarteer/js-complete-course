7cd31b67a6ed213266e5dbe8e1505013ed4c4665
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-07 10:43:24 +0200

    ex3.4

--- 03-dates/03-age-by-select/script.js
+++ 03-dates/03-age-by-select/script.js
@@ -12,7 +12,6 @@
 
 // your code here
 document.getElementById('run').addEventListener("click", getAge);
-
 function getAge() {
     const dateNow = new Date();
     console.log(dateNow)
@@ -23,15 +22,21 @@ function getAge() {
     if (monthNow < DbMonth) {
         yearNow--;
     }
+    /*If the current month is less than the birth month, the current year will not be counted.
+    To get the month's difference, we will subtract by adding the total number of months (12) to the current month.*/
     const dayNow = dateNow.getDay();
     const DbDay = document.getElementById('dob-day').value;
     if (dayNow < DbDay) {
         monthNow--;
     }
+    /*If the current date is less than the date (birthdate) entered by the user,
+    that month will not be counted as the month is not completed. Otherwise, we
+    will add the number of month days (30 or 31) to the current date to get the difference between them.*/
     const currentYear = new Date(yearNow, monthNow, dayNow)
     const BdayDate = new Date(DbYear, DbMonth, DbDay);
     console.log("birthday date " + BdayDate)
     const age = currentYear.getFullYear() - BdayDate.getFullYear()
+    /*Finally, we just need to subtract the date, month and year after satisfying the above two conditions.*/
     console.log("calculation years: " + age)
     let msg = ("You are " + age + " years old!")
     alert(msg);
--- 03-dates/04-locale-date-Optional/script.js
+++ 03-dates/04-locale-date-Optional/script.js
@@ -9,10 +9,16 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+// to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
-    // your code here
+// your code here
+/*https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings*/
+const options = {weekday: 'long', day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit'};
+const now = new Date().toLocaleDateString("en-BE", options).replace(',', '')
+    .replace(':', 'h');
+document.getElementById("target").innerHTML = now;
 
-})();
+console.log(now)
+console.log(options)
+console.log(new Date)
