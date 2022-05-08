9fcd60fbe4d6cd2de0206415199a1e1603ac39a7
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-08 13:50:30 +0200

    parseInt added to ex4.1

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -10,32 +10,32 @@
 // You will have time to focus on it later.
 document.getElementById("addition").addEventListener("click", add);
 function add() {
-    let one = document.getElementById("op-one").value
-    let two = document.getElementById("op-two").value
+    let one = parseInt(document.getElementById("op-one").value)
+    let two = parseInt(document.getElementById("op-two").value)
     let addIs = (one + two);
     let msg = (addIs);
     alert(msg);
 }
 document.getElementById("substraction").addEventListener("click", substract);
 function substract() {
-    let one = document.getElementById("op-one").value
-    let two = document.getElementById("op-two").value
+    let one = parseInt(document.getElementById("op-one").value)
+    let two = parseInt(document.getElementById("op-two").value)
     let subIs = (one - two);
     let msg = (subIs);
     alert(msg);
 }
 document.getElementById("multiplication").addEventListener("click", multiply);
 function multiply() {
-    let one = document.getElementById("op-one").value
-    let two = document.getElementById("op-two").value
+    let one = parseInt(document.getElementById("op-one").value)
+    let two = parseInt(document.getElementById("op-two").value)
     let multiIs = (one * two);
     let msg = (multiIs);
     alert(msg);
 }
 document.getElementById("division").addEventListener("click", divide);
 function divide() {
-    let one = document.getElementById("op-one").value
-    let two = document.getElementById("op-two").value
+    let one = parseInt(document.getElementById("op-one").value)
+    let two = parseInt(document.getElementById("op-two").value)
     let diviIs = (one / two);
     let msg = (diviIs);
     alert(msg);
