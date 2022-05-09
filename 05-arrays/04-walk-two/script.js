a22daa24f516e8bec76af91852580184b1c6ab03
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-09 10:32:16 +0200

    ex5.4

--- 05-arrays/04-walk-two/script.js
+++ 05-arrays/04-walk-two/script.js
@@ -11,7 +11,7 @@
 
 (function() {
 
-    var fruits = [
+    let fruits = [
         "pomme",
         "poire",
         "fraise",
@@ -27,5 +27,9 @@
     ];
 
     // your code here
-
+    document.getElementById("run").addEventListener("click", function () {
+        fruits.forEach(function (value) {
+           console.log(value);
+        })
+    })
 })();
