433500f00c8773595389aa6a77e4e16f3f98fa7b
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-11 14:19:10 +0200

    ex6.3

--- 06-dom/03-select-three/script.js
+++ 06-dom/03-select-three/script.js
@@ -9,8 +9,11 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
 
     // your code here
+let targ = document.getElementsByClassName("target") ;
+for (let i = 0; i < targ.length; i++) {
+    targ[i].innerHTML = "owned"
+}
 
-})();
