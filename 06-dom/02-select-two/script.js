e9a88c90dde9dfc7146983906665851cdebc0b70
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-11 13:59:29 +0200

    ex6.2

--- 06-dom/02-select-two/script.js
+++ 06-dom/02-select-two/script.js
@@ -9,8 +9,11 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
 
     // your code here
 
-})();
+document.querySelector("#target").innerHTML = "owned";
+
+
+
