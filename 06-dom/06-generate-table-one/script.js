1dd3a307fb6be2548b6e4eff412d78247a0df156
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-12 09:43:10 +0200

    ex6.6

--- 06-dom/06-generate-table-one/script.js
+++ 06-dom/06-generate-table-one/script.js
@@ -9,8 +9,24 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+// your code here
+//https://www.w3schools.com/jsref/dom_obj_table.asp
 
-})();
+
+let targ = document.getElementById("target");
+console.log(targ);
+
+//create new element table.
+    const table = document.createElement("table");
+    console.log(table);
+//create 10 rows (automatically added to the table); in each row one cell will be inserted.
+    for (let i = 0; i < 10; i++) {
+        table.insertRow(i).insertCell()
+    }
+//Insert the table inside the targ.
+    targ.appendChild(table);
+
+
+
+
