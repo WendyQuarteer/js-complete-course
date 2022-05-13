36ca262aed13963019616c59729b9322e815acc3
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-13 10:08:49 +0200

    NEW ex7 & optional

---
+++ 07-fetch/01-list-to-console/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /09-fetch/01-list-to-console/script.js - 11.1: liste vers console
+ *
+ * coded by leny@BeCode
+ * started at 12/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+    // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        getGithubData();
+    })
+    async function getGithubData() {
+        //await operator is used to wait for a Promise
+        let data = await fetch('../../_shared/api.json');
+        let main = await data.json();
+        console.log(main);
+    }
+})();
